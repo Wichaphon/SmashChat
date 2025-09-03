549537c3a0cd62d1d4133f9077c4c3426d6c9577
Wichaphon <horokindov3310@gmail.com>
2025-09-03 11:18:03 +0700

    fixed error connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,33 +1,42 @@
 import express from "express";
-
-import cors from 'cors';
-
-import authRoutes from "./routes/auth.route.js"
-import messageRoutes from "./routes/message.route.js"
-
 import dotenv from "dotenv";
-import { connnectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
+import path from "path";
 
-dotenv.config()
-const app = express();
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
+
+dotenv.config();
+
 const PORT = process.env.PORT;
-
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
+app.use(
+  cors({
     origin: "http://localhost:5173",
     credentials: true,
-}));
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
-    console.log("Server is running on port: " + PORT)
-    connnectDB();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });
